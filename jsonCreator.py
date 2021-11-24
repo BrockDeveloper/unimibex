8208ac05869a4ea322daeb6b923a6685f5f463df
TechAle <alessandro.condello.email@gmail.com>
2021-11-25 00:22:07 +0100

    [ADD] consoleUtils and added the option of choosing previous links

--- jsonCreator.py
+++ jsonCreator.py
@@ -25,7 +25,7 @@
     - La persona può scegliere più anni accademici e più giorni
 """
 
-from utils import dataUtils, cryptUtils
+from utils import dataUtils, cryptUtils, consoleUtils
 import json
 
 
@@ -34,9 +34,9 @@ def requestFromKeys(keys):
         for i, key in enumerate(keys.keys()):
             print("%s) %s" % (i + 1, key))
 
-        choose = int(input("Choose: ")) - 1
-        if -1 < choose < keys.__len__():
-            return list(keys.keys())[choose]
+        chooseKey = int(input("Choose: ")) - 1
+        if -1 < chooseKey < keys.__len__():
+            return list(keys.keys())[chooseKey]
 
 
 def requestFromList(_list):
@@ -44,9 +44,9 @@ def requestFromList(_list):
         for i, key in enumerate(_list):
             print("%s) %s" % (i + 1, key["label"]))
 
-        choose = int(input("Choose: ")) - 1
-        if -1 < choose < _list.__len__():
-            return _list[choose]
+        chooseList = int(input("Choose: ")) - 1
+        if -1 < chooseList < _list.__len__():
+            return _list[chooseList]
 
 
 def getClassesFromSchool(dataset, schoolDataset):
@@ -59,12 +59,35 @@ def requestKindOfStudy(classesDataset):
         for i, key in enumerate(classesDataset):
             print("%s) %s - %s (%s)" % (i + 1, key["valore"], key["label"], key["tipo"]))
 
-        choose = int(input("Choose: ")) - 1
-        if -1 < choose < classesDataset.__len__():
-            return classesDataset[choose]
+        chooseStudy = int(input("Choose: ")) - 1
+        if -1 < chooseStudy < classesDataset.__len__():
+            return classesDataset[chooseStudy]
 
 
 def askLinks(subjectsDataset):
+    def iterateDuplicates(values):
+        while True:
+            for i, key in enumerate(values):
+                print("%s) %s - %s" % (i + 1, key["teachers"], key["dayString"]))
+
+            chooseWhile = input("Choose (no or nothing for inserting new links): ")
+            if not chooseWhile.isnumeric():
+                return -1
+            chooseWhile = int(chooseWhile) - 1
+            if -1 < chooseWhile < values.__len__():
+                return values[chooseWhile]
+
+    def isHere(dataset, now):
+        values = [x for x in dataset if x["lesson"] == now["lesson"]]
+
+        if values.__len__() > 0:
+            print("\nFound previos servers")
+            val = iterateDuplicates(values)
+            if val != -1:
+                return val
+
+        return False
+
     output = []
     for subject in subjectsDataset:
         print("Lesson: %s\n Day: %s, start: %s, end: %s" % (
@@ -73,15 +96,32 @@ def askLinks(subjectsDataset):
         print("Teachers: ", end='')
         for teacher in subject["teachers"]:
             print(teacher, end=', ')
-        link = input("\nLink (no for skipping this): ")
-        if link != "no" and link.__len__() > 0:
-            subject["link"] = link
 
-            password = input("Password (empty if nothing): ")
+        skip = isHere(output, subject)
+
+        if not skip:
+            link = input("\nLink (no for skipping this): ")
+
+        # noinspection PyUnboundLocalVariable
+        if skip or link != "no" and link.__len__() > 0:
+            # noinspection PyUnresolvedReferences
+            subject["link"] = skip["link"] if skip else link
+
+            password = ""
+
+            if skip:
+                # noinspection PyUnresolvedReferences
+                if skip.keys().__contains__("password"):
+                    # noinspection PyUnresolvedReferences
+                    password = skip["password"]
+            else:
+                password = input("Password (empty if nothing): ")
+
             if password.__len__() != 0:
                 subject["password"] = password
 
             output.append(subject)
+        consoleUtils.clear()
     return output
 
 
@@ -113,41 +153,51 @@ if __name__ == "__main__":
     while True:
 
         year = requestFromKeys(years)
+        consoleUtils.clear()
         if year.__len__() == 0:
             continue
 
         informations = dataUtils.getUniversityInformations(urls["courses"], year)
+        consoleUtils.clear()
         if informations.__len__() == 0:
             continue
 
         school = requestFromKeys(informations["schools"])
+        consoleUtils.clear()
         if school.__len__() == 0:
             continue
 
         classes = getClassesFromSchool(informations["classes"], informations["schools"][school])
+        consoleUtils.clear()
         if classes.__len__() == 0:
             continue
 
         kindStudy = requestKindOfStudy(classes)
+        consoleUtils.clear()
         if kindStudy.__len__() == 0:
             continue
 
         listCourses = requestFromList(kindStudy["elenco_anni"])
+        consoleUtils.clear()
         if listCourses.__len__() == 0:
             continue
 
         subjects = dataUtils.getSubjects(urls["classes"], urls["params"], year, listCourses,
                                          informations["schools"][school], kindStudy["valore"])
+        consoleUtils.clear()
         if subjects.__len__() == 0:
             continue
 
         subjectsWithLink = askLinks(subjects)
+        consoleUtils.clear()
         if subjectsWithLink.__len__() == 0:
             continue
 
         toSave.extend(subjectsWithLink)
         choose = input("Save or continue? (Save/Continue)")
+        consoleUtils.clear()
 
         if choose.lower() == "save":
+            print("Saving in progress..")
             save(toSave)
             toSave.clear()
